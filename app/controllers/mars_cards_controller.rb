d15325bad406dc889d7404c1982e39f3fc402575
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-11 15:55:13 -0400

    show method for mars cards

--- app/controllers/mars_cards_controller.rb
+++ app/controllers/mars_cards_controller.rb
@@ -6,5 +6,11 @@ class MarsCardsController < ApplicationController
         {:except => [:created_at, :updated_at]})
 
     end
+    def  show
+        mars_card=MarsCard.find(params[:id])
+        render json: mars_card.to_json(
+        {:except => [:created_at, :updated_at]})   
+    end
+   
 
 end
