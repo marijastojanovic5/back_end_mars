7cd52947d690aa72971002ed84c2b7a11cac816d
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-19 17:02:52 -0400

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :favorites
     has_many :comments
     has_many :mars_cards, through: :favorites
+    validates :username, uniqueness: { case_sensitive: false }, presence: true
    
 end
