77e897e103c26b32dda2b6e63fa4c4277d2290fe
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-19 12:50:41 -0400

    comments controller, creating done

--- app/models/mars_card.rb
+++ app/models/mars_card.rb
@@ -3,11 +3,5 @@ class MarsCard < ApplicationRecord
     has_many :comments
     has_many :users, through: :favorites
 
-
-    def self.names
-        MarsCard.all.map do |card|
-            card.name
-        end.uniq
-    end
     
 end
