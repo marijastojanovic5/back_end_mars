1853513f18f9adf5ed7fd67db49368277f9d917e
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-21 18:29:52 -0400

    deleting comments almost done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,17 @@ def create
     render json: { mars_card_id: comment.mars_card, comment: comment.comment} 
 
 end
+def destroy
+    
+    user_id=params[:user_id]
+    mars_card_id=params[:mars_card_id]
+    comment=params[:comment]
+    comment1 = Comment.find_by(:user_id=>user_id, :mars_card_id=>mars_card_id,:comment=> comment)
+    comment1.destroy
+
+
+    
+end
     
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   post '/login', to: 'users#login_create'
+  
   resources :comments,only: [:create]
+  delete '/comments/:user_id/:mars_card_id', to: "comments#destroy"
+
   resources :favorites, only: [:create]
   delete '/favorites/:user_id/:mars_card_id', to: "favorites#destroy"
   resources :mars_cards,only: [:index, :show]
